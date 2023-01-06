e398c17566ced709da40d79c7499d4b154a7b1e5
Pratik Moitra <moitra.pratik@gmail.com>
2023-01-06 14:10:59 +0100

    Commit time: Fri Jan  6 14:10:59 CET 2023, by dogit.

---
+++ boilerscript.sh
@@ -0,0 +1,21 @@
+
+#!/bin/bash
+
+# Install Ansible on Arch Linux
+if [ -f /etc/arch-release ]; then
+    sudo pacman -S ansible
+fi
+
+# Install Ansible on Debian/Ubuntu
+if [ -f /etc/debian_version ]; then
+    sudo apt update
+    sudo apt install ansible
+fi
+
+# Install ansible on macOS
+if [ "$(uname)" == "Darwin" ]; then
+    brew install ansible
+fi
+
+# Run ansible-pull command to execute an ansible script. 
+sudo ansible-pull -U https://github.com/PratikMoitra/ansible-files.git playbook.yml
